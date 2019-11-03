8e0f8186d39f3ddee76b071ab75d37ebfcb3816e
randomlylelo <38267386+randomlylelo@users.noreply.github.com>
2019-11-03 10:04:37 -0500

    4:21:00; Added new File

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 /* 
 void main(){
@@ -14,7 +15,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MyAppState();
   }
 }
@@ -47,23 +47,27 @@ class _MyAppState extends State<MyApp> {
             Question(
               questions[_questionIndex],
             ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed:
-                  _answerQuestion, // Passing the refrence to onPressed bc onPressed wants function not return value of the function.
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print(
-                  'Shorthand for function, this is also called a anonymous function (function with no name)'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                // This is used for multi line functions, another anonymous function bc it doesn't have a name.
-                print('Another shorthand way :)');
-              },
-            ),
+            Answer(),
+            Answer(),
+            Answer(),
+            // Ctrl + K + C to comment, Ctrl + K + U to uncomment
+            // RaisedButton(
+            //   child: Text('Answer 1'),
+            //   onPressed:
+            //       _answerQuestion, // Passing the refrence to onPressed bc onPressed wants function not return value of the function.
+            // ),
+            // RaisedButton(
+            //   child: Text('Answer 2'),
+            //   onPressed: () => print(
+            //       'Shorthand for function, this is also called a anonymous function (function with no name)'),
+            // ),
+            // RaisedButton(
+            //   child: Text('Answer 3'),
+            //   onPressed: () {
+            //     // This is used for multi line functions, another anonymous function bc it doesn't have a name.
+            //     print('Another shorthand way :)');
+            //   },
+            // ),
           ],
         ),
       ),
